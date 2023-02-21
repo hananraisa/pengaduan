f0552dfb47cbb1d320da9c40cc1d19cb22052112
hanan <hananhar09@gmail.com>
2023-02-21 12:08:14 +0700

    make migration

---
+++ database/migrations/2023_02_21_043757_create_pengaduan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengaduan', function (Blueprint $table) {
+            $table->id('id_pengaduan');
+            $table->string('tgl_pengaduan');
+            $table->string('nik');
+            $table->string('isi_laporan');
+            $table->string('foto');
+            $table->enum('status',['0','proses','selesai']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengaduan');
+    }
+};
---
+++ database/migrations/2023_02_21_044709_create_tanggapan_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tanggapan', function (Blueprint $table) {
+            $table->id('id_tanggapan');
+            $table->string('tgl_tanggapan');
+            $table->string('tanggapan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tanggapan');
+    }
+};
